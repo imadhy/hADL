1143d32401ab158c17cca893fe588cdc8b48666e
imadhy <imadhy.gt@gmail.com>
2015-11-30 14:27:53 +0100

    Add project hADL

---
+++ src/main/java/com/univ/alma/csa/App.java
@@ -0,0 +1,13 @@
+package com.univ.alma.csa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
